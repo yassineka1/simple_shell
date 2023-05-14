1ed7fc4a3c0e8814960796766744e64e89382d24
yassineka1 <yassine.kabbaj91@gmail.com>
2023-05-14 05:16:12 -0700

    Beautiful &  Simple code that passes the Betty checks

---
+++ yassine_and_reda.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * beautiful_addition - Function that adds four integers
+ *			and is betty compliant.
+ * @i1: First integer
+ * @i2: Second integer
+ * @i3: Third integer
+ * @i4: Fourth integer
+ *
+ * Description: give seven integers and returns sum
+ *
+ * Return: Returns the sum of the given integers
+ */
+
+int beautiful_addition(int i1, int i2, int i3, int i4)
+{
+	int sum;
+
+	sum = i1 + i2 + i3 + i4;
+
+	return (sum);
+}
